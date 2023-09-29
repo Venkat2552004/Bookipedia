47067fed8be132fa5c370fe3278597f4c9c09253
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2023-09-29 10:08:26 +0530

    Add modified Home page, script and style files.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-$(document).ready(function () {
-    $('.nav > li').mouseenter(function () {
-            //e.preventDefault();
-            $('.nav > li').removeClass('active');
-            $(this).addClass('active');              
-                               
-    });  
+
+//funtion to redirect to the course page
+function searchCourse() {
+    const courseCode = document.getElementById('searchInput').value;
+    // Redirect to the CSE.html page with the course code as a query parameter
+    window.location.href = `CSE.html?code=${courseCode}`;
+}
+
+// Code for highlighting the row on the semester page
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the course code from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const courseCode = urlParams.get('code');
+
+    // Find and highlight the corresponding row
+    const courseRow = document.querySelector(`.course-row[data-course-code="${courseCode}"]`);
+    if (courseRow) {
+        courseRow.style.backgroundColor = 'yellow'; // Highlight the row
+        // You can also scroll to the highlighted row if needed
+        courseRow.scrollIntoView({ behavior: 'smooth' });
+    }
 });
