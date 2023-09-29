6428fdc8bd8750e5230036d50c4876f1b94ffbf0
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2023-09-29 15:10:05 +0530

    Add features for course search, highlight and autoscroll.

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 
-//funtion to redirect to the course page
-function searchCourse() {
-    const courseCode = document.getElementById('searchInput').value;
-    // Redirect to the CSE.html page with the course code as a query parameter
-    window.location.href = `CSE.html?code=${courseCode}`;
-}
-
-// Code for highlighting the row on the semester page
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the course code from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const courseCode = urlParams.get('code');
-
-    // Find and highlight the corresponding row
-    const courseRow = document.querySelector(`.course-row[data-course-code="${courseCode}"]`);
-    if (courseRow) {
-        courseRow.style.backgroundColor = 'yellow'; // Highlight the row
-        // You can also scroll to the highlighted row if needed
-        courseRow.scrollIntoView({ behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', () =>{
+    let isIndex = false;
+    let isDept = false;
+    if(document.title === 'Home'){
+        isIndex = true;
+    }
+    else{
+        isDept = true;
+    }
+    if(isIndex){
+        const searchIcon = document.getElementById('sbutton');
+        searchIcon.addEventListener('click', () => {
+            const searchData = document.getElementById('sbar').value;
+            const [courseCode, dept] = searchData.split('-').map(str => str.toUpperCase());
+            window.location.href = `${dept}.html?code=${courseCode}`;
+        });
+    }
+    if(isDept){
+        const urlParams = new URLSearchParams(window.location.search);
+        const courseCode = urlParams.get('code');
+        const courseRow = document.getElementById(courseCode);
+        if (courseRow) {
+            courseRow.classList.add('highlighted');
+            courseRow.scrollIntoView({ behavior: 'smooth' });
+            setTimeout(() => {
+                courseRow.classList.remove('highlighted');
+            }, 5000);
+        }
     }
 });
